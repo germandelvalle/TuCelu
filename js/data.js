9fcf4717d1ca7f9a7e2ffa154a771f7ef7efb6ef
German Del Valle <delvalle.german.a@gmail.com>
2020-11-10 11:39:45 -0300

    trabajo 8 y 9 de js finalizado

---
+++ js/data.js
@@ -0,0 +1,56 @@
+const data = [
+    {
+        id: 0,
+        nombre: 'Iphone 8 ',
+        imagen: 'images/iphone8plusitem.jpg',
+        imagen2: 'images/iphone-8-plus.jpg',
+        descipcion: {
+            fecha: 'Lanzamiento: Septiembre 2017',
+            peso: 'PESO:202g',
+            espesor: 'ESPESOR:7.5mm',
+            memoria: 'MEMORIA:64GB',
+            pantalla: 'PANTALLA:5.5″1080×1920 pixels',
+            camara: '1CAMARA:12MP 2160p',
+            RAM: 'MEMORIA RAM:3GB',
+            procesador:'PROCESADOR:Apple A11 Bionic',
+            bateria: 'BATERIA:2691mAhLi-Ion'
+        },
+        precio: 80000
+    },
+    {
+        id: 1,
+        nombre: 'iPhone X',
+        imagen: 'images/iphonex.jpg',
+        imagen2: 'images/photo-1523206489230-c012c64b2b48.jpeg',
+        descipcion: {
+            fecha: 'Lanzamiento Noviembre 2017',
+            peso: 'Peso 174 gramos',
+            espesor: 'Espesor  7.7mm ',
+            memoria: 'Memoria interna 64/256GB No Expandible',
+            pantalla: 'Pantalla de 5.8″',
+            camara: 'Camara Principal 12MP',
+            RAM: 'Memoria Ram 3GB',
+            procesador:'procesador Apple A11 (6 núcleos)',
+            bateria: 'Bateria 2716mAh Li-Ion'
+        },
+        precio: 140000
+    },
+    {
+        id: 3,
+        nombre: 'iPhone 11',
+        imagen: 'images/iphone112.jpg',
+        imagen2: 'images/iphone11.jpg',
+        descipcion: {
+            fecha: 'Lanzamiento septiembre 2019',
+            peso: 'Peso 194 g',
+            espesor: 'Espesor  8,3 mm ',
+            memoria: 'Memoria interna 128 GB',
+            pantalla: 'Pantalla de 6,1"',
+            camara: 'Camara Principal 12MP',
+            RAM: 'Memoria Ram 4 GB',
+            procesador:'procesador Chip A13 Bionic',
+            bateria: 'Carga rápida 18W (cargador no incluido)'
+        },
+        precio: 190000
+    }
+]
---
+++ js/index.js
@@ -0,0 +1,119 @@
+let url = window.location.href
+console.log(url)
+
+const $header = 
+`
+<nav class="navbar navbar-expand-lg navbar-light position-sticky">
+    <h1 class="navbar-brand w-50" href="#">TuCelu</h1>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav w-100 d-flex justify-content-around">
+        <li class="nav-item">
+        <a class="nav-link" href="index.html">Home</a>
+        </li>
+        <li class="nav-item">
+        <a class="nav-link" href="about.html">Acerca de</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="contact.html">Contacto</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="registrer.html">Registrate</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="locals.html">Sucursales</a>
+        </li>
+    </ul>
+    </div>
+</nav>
+`
+
+const $footer = 
+`
+<section class="footer__redes">
+    <a class="footer__redes--items" href="https://www.instagram.com/"><i class="fab fa-instagram"></i></a>
+    <a class="footer__redes--items" href="https://www.facebook.com/"><i class="fab fa-facebook-square"></i></a>
+    <a class="footer__redes--items" href="https://www.linkedin.com/"><i class="fab fa-linkedin"></i></a>
+    <a class="footer__redes--items" href="https://www.twitter.com/"><i class="fab fa-twitter"></i></a>
+</section>
+<p class="footer__derechos">todos los derechos reservados</p>
+`
+
+let product=''
+for(let i = 0; i<data.length;i++){
+    product += `
+    <article class="menu__products--items">
+        <img src="${data[i].imagen2}" alt="/">
+        <section class="menu--items__description">
+            <h2>${data[i].nombre}</h2>
+
+            <a class="menu--items__description--more" href=${posicion(i)}>ver mas</a>
+
+        </section>
+    </article>
+    `
+}
+
+function posicion(pos){
+    switch(pos){
+        case 0: return "iphone8.html"
+        case 1: return "iphone10.html"
+        case 2: return "iphone11.html"
+    }
+}
+
+let cards = data.map((card)=>{
+    return(
+        `
+        <img class="menu__article--image" src="${card.imagen}" alt="">
+        <section class="menu__article--description">
+
+            <h2>${card.nombre}</h2>
+            <ul>
+                <li>${card.descipcion.fecha}</li>
+                <li>${card.descipcion.peso}</li> 
+                <li>${card.descipcion.espesor}</li> 
+                <li>${card.descipcion.memoria}</li> 
+                <li>${card.descipcion.pantalla}</li> 
+                <li>${card.descipcion.camara}</li> 
+                <li>${card.descipcion.RAM}</li> 
+                <li>${card.descipcion.procesador}</li>
+                <li>${card.descipcion.bateria}</li>  
+            </ul>
+            <h3>$ ${card.precio}</h3>
+            <button class="buyButtom">Aniadir al carrito</button>
+
+
+        </section>
+        `
+    )
+})
+
+
+document.getElementById('header').innerHTML=$header
+document.getElementById('footer').innerHTML=$footer
+
+if(url == 'http://127.0.0.1:5500/index.html'){
+    document.getElementById('products').innerHTML=product;
+}
+if(url == 'http://127.0.0.1:5500/iphone8.html'){
+    document.getElementById('article0').innerHTML=cards[0];
+}
+if(url == 'http://127.0.0.1:5500/iphone10.html'){
+    document.getElementById('article1').innerHTML=cards[1];
+}
+if(url == 'http://127.0.0.1:5500/iphone11.html'){
+    document.getElementById('article2').innerHTML=cards[2];
+}
+// switch(url){
+//     case : document.getElementById('products').innerHTML=product;
+//     case 'http://127.0.0.1:5500/iphone8': document.getElementById('article0').innerHTML=cards[0];
+// }
+
+
+
+
+
+
